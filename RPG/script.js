f421bbf2778fd4076c42762b3a69f15532169d70
Jeffin03 <jeffyissac2203@gmail.com>
2023-12-29 12:19:27 +0530

    update RPG

--- RPG/script.js
+++ RPG/script.js
@@ -25,7 +25,7 @@ const weapons = [
 
 const monsters = [
   {
-    name:slime,
+    name:'slime',
     level:2,
     health:15
   },
@@ -35,7 +35,7 @@ const monsters = [
     health: 60,
   },
   {
-  name:dragon,
+  name:'dragon',
     level: 20,
     health:300,
   }
@@ -59,7 +59,64 @@ const locations = [
         "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
         "button functions": [fightSlime, fightBeast, goTown],
         text: "You enter the cave. You see some monsters."
-    }
+    },
+  {
+    name:"fight",
+    "button text": [
+      "Attack",
+      "Dodge",
+      "Run"
+    ],
+    "button functions":[
+      attack,
+      dodge,
+      goTown
+    ],
+    text: "You are fighting a monster."
+  },
+  {
+    name:"kill monster",
+    "button text":[
+      "Go to town square",
+      "Go to town square",
+      "Go to town square"
+    ],
+    "button functions":[
+      goTown,
+      goTown,
+      goTown
+    ],
+    text:'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
+  },
+  {
+    name:"lose",
+    "button text":[
+      "REPLAY?",
+      "REPLAY?",
+      "REPLAY?"
+    ],
+    "button functions":[
+      restart,
+      restart,
+      restart
+    ],
+    text:"You die. ☠️"
+  },
+  {
+    name:"win",
+    "button text":[
+      "REPLAY?",
+      "REPLAY?",
+      "REPLAY?"
+    ],
+    "button functions":[
+      restart,
+      restart,
+      restart
+    ],
+    text:"You defeat the dragon! YOU WIN THE GAME! 🎉"
+  }
+
 ];
 
 // initialize buttons
@@ -68,6 +125,7 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location) {
+  monsterStats.style.display = "none";
   button1.innerText = location["button text"][0];
   button2.innerText = location["button text"][1];
   button3.innerText = location["button text"][2];
@@ -132,14 +190,73 @@ function sellWeapon() {
   }
 }
 
-function fightSlime() {
+function goFight(){
+  update(locations[3]);
+  monsterHealth = monsters[fighting].health;
+  monsterStats.style.display = block;
+  monsterName.innerText = monsters[fighting].name;
+  monsterHealthText.innerText = monsterHealth;
+}
 
+function fightSlime() {
+  fighting = 0;
+  goFight();
 }
 
 function fightBeast() {
-
+  fighting = 1;
+  goFight();
 }
 
 function fightDragon() {
-  console.log("Fighting dragon.");
+  fighting= 2;
+  goFight();
+}
+
+function attack(){
+  if(health <=0){
+    lose();
+  } else if(monsterHealth <= 0){
+    fighting === 2? winGame():defeatMonster();
+  }
+  text.innerText = "The "+ monsters[fighting].name+" attacks.";
+  text.innerText += " You attack it with your "+ weapons[currentWeapon].name+".";
+  health -= getMonsterAttackValue(monsters[fighting].level);
+  monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+  healthText.innerText = health;
+  monsterHealthText.innerText = monsterHealth;
+ 
+}
+
+function dodge(){
+ text.innerText = "You dodge the attack from the " + monsters[fighting].name + ".";
+}
+
+function defeatMonster(){
+  gold += Math.floor(monsters[fighting].level * 6.7);
+  xp += monsters[fighting].level;
+  goldText.innerText = gold;
+  xpText.innerText = xp;
+  update(locations[4]);
+
+}
+
+function lose(){
+  update(locations[5]);
+}
+
+function winGame(){
+  update(locations[6]);
+}
+
+function restart(){
+  xp = 0;
+  health = 100;
+  gold = 50;
+  currentWeapon = 0;
+  inventory = ['stick'];
+  goldText.innerText = gold;
+  healthText.innerText = health;
+  xpText.innerText = xp;
+  goTown();
 }
