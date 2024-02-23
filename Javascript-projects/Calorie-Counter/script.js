3e2032f5c7ea3f6e99a82dde1f7c40e046643d64
Jeffin03 <jeffyissac2203@gmail.com>
2024-02-23 23:33:30 +0530

    update

--- Javascript-projects/Calorie-Counter/script.js
+++ Javascript-projects/Calorie-Counter/script.js
@@ -1,14 +1,14 @@
-const calorieCounter = document.getElementById("calorie-counter");
-const budgetNumberInput = document.getElementById("budget");
-const entryDropdown = document.getElementById("entry-dropdown");
-const addEntryButton = document.getElementById("add-entry");
-const clearButton = document.getElementById("clear");
-const output = document.getElementById("output");
+const calorieCounter = document.getElementById('calorie-counter');
+const budgetNumberInput = document.getElementById('budget');
+const entryDropdown = document.getElementById('entry-dropdown');
+const addEntryButton = document.getElementById('add-entry');
+const clearButton = document.getElementById('clear');
+const output = document.getElementById('output');
 let isError = false;
 
 function cleanInputString(str) {
   const regex = /[+-\s]/g;
-  return str.replace(regex, "");
+  return str.replace(regex, '');
 }
 
 function isInvalidInput(str) {
@@ -17,8 +17,6 @@ function isInvalidInput(str) {
 }
 
 function addEntry() {
-  const targetId = "#" + entryDropdown.value;
-  const targetInputContainer = document.querySelector(
-    targetId + " .input-container"
-  );
+  const targetId = '#' + entryDropdown.value;
+  const targetInputContainer = document.querySelector(targetId + ' .input-container');
 }
